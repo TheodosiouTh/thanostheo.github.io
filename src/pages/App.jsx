deb2835be90bd3cd12f3b1462b0eba1894170f2e
Thanos Theodosiou <thanosthd@gmail.com>
2021-04-12 22:11:48 +0300

    Add main page to house app

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './App.module.scss';
 
+import Main from './Main/Main';
+
 function App() {
   return (
     <div className={styles.App}>
-      <p>Hello world!</p>
+      <Main />
     </div>
   );
 }
