d6937dbbadf7fe765e56a94e23c85aa1318df70f
Thanos Theodosiou <thanosthd@gmail.com>
2021-04-17 17:38:11 +0300

    Add basic structure to about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,5 +3,10 @@ import React from 'react';
 import styles from './About.module.scss';
 
 export default function Index() {
-  return <div className={styles.container}>About</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.pageHead}>About</div>
+      <div className={styles.pageContent}>Content</div>
+    </div>
+  );
 }
