fa6ca5c5e40e2c134ac46f1d5131c3620fae20b1
Thanos Theodosiou <thanosthd@gmail.com>
2021-04-17 21:09:05 +0300

    Add toggle button and make navigation responsive for mobile

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 
 /* ICONS */
 import {
@@ -6,28 +7,48 @@ import {
   Computer as ComputerIcon,
   Note as NoteIcon,
   Info as InfoIcon,
+  Menu as MenuIcon,
 } from '@material-ui/icons';
 
 /* SUB-COMPONENTS */
+import { IconButton } from '@material-ui/core';
 import { IconRoute } from '../Links/Links';
 
 import styles from './Navigation.module.scss';
 
 export default function Sidemenu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <IconRoute title="Index" route="/">
-        <HomeIcon />
-      </IconRoute>
-      <IconRoute title="Projects" route="/projects">
-        <ComputerIcon />
-      </IconRoute>
-      <IconRoute title="Blog" route="/blogs">
-        <NoteIcon />
-      </IconRoute>
-      <IconRoute title="About Me" route="/about">
-        <InfoIcon />
-      </IconRoute>
-    </div>
+    <>
+      <div className={styles.toggle}>
+        <IconButton
+          onClick={() => {
+            setIsMenuOpen(!isMenuOpen);
+          }}
+          aria-label="Toggle Menu"
+        >
+          <MenuIcon />
+        </IconButton>
+      </div>
+      <div
+        className={cn(styles.container, {
+          [styles.open]: isMenuOpen,
+        })}
+      >
+        <IconRoute title="Index" route="/">
+          <HomeIcon />
+        </IconRoute>
+        <IconRoute title="Projects" route="/projects">
+          <ComputerIcon />
+        </IconRoute>
+        <IconRoute title="Blog" route="/blogs">
+          <NoteIcon />
+        </IconRoute>
+        <IconRoute title="About Me" route="/about">
+          <InfoIcon />
+        </IconRoute>
+      </div>
+    </>
   );
 }
