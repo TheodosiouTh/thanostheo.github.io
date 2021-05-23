6d1d675f91e3052d2d76f8292cce33b0d78730fc
Thanos Theodosiou <thanosthd@gmail.com>
2021-05-23 12:56:28 +0300

    Main page should be the index page App page should be the page to handle everything

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './App.module.scss';
 
+import Navigation from '../components/Navigation/Navigation';
+
 import Main from './Main/Main';
 
 function App() {
   return (
     <div className={styles.App}>
+      <Navigation />
       <Main />
     </div>
   );
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import styles from './Main.module.scss';
-
-/* COMPONENTS */
-import Navigation from '../../components/Navigation/Navigation';
-
-/* PAGES */
-import About from '../About/About';
 
 export default function Index() {
   return (
     <div className={styles.container}>
-      <Router>
-        <Navigation />
-        <Switch>
-          <Route path="/projects">Projects</Route>
-          <Route path="/blogs">Blogs</Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">Main</Route>
-        </Switch>
-      </Router>
+      Hello
     </div>
   );
 }
