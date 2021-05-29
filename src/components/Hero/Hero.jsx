6459f9deed40f1f27888ed328157598086d88d86
Thanos Theodosiou <thanosthd@gmail.com>
2021-05-29 19:44:24 +0300

    Add content on the Hero compontent

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import styles from './Hero.module.scss';
+
 export default function Hero() {
   return (
-    <div style={{ color: 'white' }}>
-      Hello
+    <div className={styles.container}>
+      <div className={styles.intro}>
+        Hello, my name is
+      </div>
+      <div className={styles.title}>
+        Thanos Theodosiou
+      </div>
+      <div className={styles.introduction}>
+        I am a Software engineer based in Athens who specializes in
+        developing exceptional Applications that help people stress less
+        about mundane tasks and focus more on things that matter more.
+        Currently, I am an engineer at
+        <a href="https://thinkdesquared.com/"> Desquared </a>
+        focused on making online shopping more accessible for consumers and easier
+        to manage for store managers.
+      </div>
+      <a href="mail-to:thanosthd@gmail.com">Get in touch</a>
     </div>
   );
 }
