6decc725ff296ae1f94366231e978e1e27002285
Thanos Theodosiou <thanosthd@gmail.com>
2021-06-06 14:13:56 +0300

    Hide header on mobile devices when user scrolls down

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import cn from 'classnames';
 
 import { ReactComponent as Logo } from '../../global/Logo.svg';
 
@@ -8,8 +9,18 @@ import Navigation from '../Navigation/Navigation';
 import styles from './Header.module.scss';
 
 export default function Sidemenu() {
+  const [shouldHide, setShouldHide] = useState(window.pageYOffset > 250);
+  function handleScroll() {
+    console.log(window.pageYOffset);
+    setShouldHide(window.pageYOffset > 250);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  });
+
   return (
-    <header>
+    <header className={cn({ [styles.hidden]: shouldHide })}>
       <div className={styles.leftSide}>
         <Logo className={styles.logo} />
       </div>
