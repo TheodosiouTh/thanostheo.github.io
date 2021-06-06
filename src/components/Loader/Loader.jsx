deff069fb5a10044327d1513756ec84fc6754e74
Thanos Theodosiou <thanosthd@gmail.com>
2021-06-06 16:44:30 +0300

    Remove unused valiables

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
 
 export default function CircularIndeterminate() {
   return (<div>Loading...</div>);
