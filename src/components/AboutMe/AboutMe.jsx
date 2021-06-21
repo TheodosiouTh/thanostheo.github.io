6d99020fc258e823a3aed24b511c5324a1562f77
Thanos Theodosiou <thanosthd@gmail.com>
2021-06-21 15:12:57 +0300

    tc: Typos

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -15,35 +15,41 @@ export default function AboutMe() {
       <div className={styles.content}>
         <div>
           <p>
-            Hello! My name is Thanos and I enjoy solving complex problems with efficient,
-            maintainable and scalable code.
-            My interest in software development started at the age of 14 when I decided to buy
-            <a href="https://www.goodreads.com/book/show/52094843-c" target="_blank" rel="noreferrer"> this book </a>
-            in order to learn how to program computer appications. After I finished reading the book
-            I knew that I wanted to be a software engineer, so a few years later after a
-            lot of studying ,I got my
+            Hello! My name is Thanos and I enjoy solving complex problems with
+            efficient, maintainable, and scalable code. My interest in software
+            development started at the age of 14 when I decided to buy
+            <a
+              href="https://www.goodreads.com/book/show/52094843-c"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {' '}
+              this book
+              {' '}
+            </a>
+            in order to learn how to program computer applications. After I
+            finished reading the book I knew that I wanted to be a software
+            engineer, so a few years later after a lot of studying, I got my
             {' '}
-            <strong>B.Sc. Computer Schience </strong>
-            at
+            <strong>B.Sc. Computer Science </strong>
+            at the
             {' '}
-            <a href="https://www.cs.unipi.gr/index.php?lang=en">University of Piraeus</a>
+            <a href="https://www.cs.unipi.gr/index.php?lang=en">
+              University of Piraeus
+            </a>
             .
           </p>
           <p>
             Fast-forward to today, and I
             <span>&#39;</span>
             ve had the privilege of working at one of the biggest software UX &
-            Softtware Development Agencies in Greece at products such as
+            Software Development Agencies in Greece at products such as
             {' '}
-            <a href="https://kritikos-sm.gr/">
-              Kritikos-SM
-            </a>
+            <a href="https://kritikos-sm.gr/">Kritikos-SM</a>
             {' '}
             and
             {' '}
-            <a href="https://box.gr/">
-              BOX
-            </a>
+            <a href="https://box.gr/">BOX</a>
             .
           </p>
           <p>
@@ -68,7 +74,6 @@ export default function AboutMe() {
           </div>
         </div>
       </div>
-
     </section>
   );
 }
