6f7079ede4776229084d005282a60e739d2d01f6
Thanos Theodosiou <thanosthd@gmail.com>
2021-11-06 17:41:33 +0200

    Add page for displaying blog entries

--- src/pages/BlogEntry/BlogEntry.jsx
+++ src/pages/BlogEntry/BlogEntry.jsx
@@ -3,22 +3,38 @@ import ReactMarkdown from 'react-markdown';
 
 import styles from './BlogEntry.module.scss';
 
+import SyntaxHighlighter from '../../components/CodeHighlighter/CodeHighlighter';
+
 export default function BlogEntry() {
   const [blogEntry, setBlogEntry] = useState('');
 
   const entry = 'building-a-blog';
-  const title = 'Building a blog!';
+  const title = 'Building a blog from scratch!';
+  const tags = ['react', 'javascript', 'happiness'];
 
   useEffect(() => {
-    fetch(`/blogs/${entry}/text.md`)
+    fetch(`/blog-entries/${entry}/text.md`)
       .then(data => data.text())
       .then(text => setBlogEntry(text));
   });
 
   return (
     <div className={styles.container}>
-      <h1>{title}</h1>
-      <ReactMarkdown>{blogEntry}</ReactMarkdown>
+      {tags &&
+        tags.map(tag => (
+          <a href={`/blogs?tag=${tag}`} className={styles.tag}>
+            {tag}
+          </a>
+        ))}
+      <h1 className={styles.title}>{title}</h1>
+      <ReactMarkdown
+        className={styles.textContainer}
+        components={{
+          code: SyntaxHighlighter,
+        }}
+      >
+        {blogEntry}
+      </ReactMarkdown>
     </div>
   );
 }
