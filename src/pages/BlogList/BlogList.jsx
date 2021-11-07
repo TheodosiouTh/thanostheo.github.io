949e50f80c568169325b0a99a5b600440a74b5d9
Thanos Theodosiou <thanosthd@gmail.com>
2021-11-07 11:19:06 +0200

    Add filtering for blog entries on tags

--- src/pages/BlogList/BlogList.jsx
+++ src/pages/BlogList/BlogList.jsx
@@ -2,11 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { readFile } from '../../common/utils';
 
 import Card from '../../components/Blog/Card/Card';
+import { useQuery } from '../../components/useQuery';
 
 import styles from './BlogList.module.scss';
 
 export default function BlogList() {
-  const [blogIndex, setBlogIndex] = useState(null);
+  const [blogIndex, setBlogIndex] = useState({});
+  const [filteredIndex, setFilteredIndex] = useState({});
+
+  const query = useQuery();
+
+  function filterIndexOnTag(index, tag) {
+    return Object.entries(index).reduce((accumulator, [slug, entry]) => {
+      if (entry.tags.includes(tag)) {
+        return { ...accumulator, [slug]: entry };
+      }
+      return accumulator;
+    }, {});
+  }
 
   useEffect(() => {
     readFile(
@@ -22,11 +35,20 @@ export default function BlogList() {
     );
   }, []);
 
+  useEffect(() => {
+    const selectedTag = query.get('tag');
+    if (!selectedTag) {
+      setFilteredIndex(blogIndex);
+      return;
+    }
+    setFilteredIndex(filterIndexOnTag(blogIndex, selectedTag));
+  }, [blogIndex, query]);
+
   return (
     <div>
       <h1 className={styles.pageTitle}>Articles</h1>
-      {blogIndex &&
-        Object.entries(blogIndex).map(([slug, entry]) => (
+      {filteredIndex &&
+        Object.entries(filteredIndex).map(([slug, entry]) => (
           <Card slug={slug} entry={entry} />
         ))}
     </div>
